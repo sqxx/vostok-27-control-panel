0ba7d105526772fbaebf2956423bca48e922cba0
Valery Klachkov <sqxx.kv@gmail.com>
2019-10-10 19:05:17 +1000

    Migrated BluetoothState to BluetoothStatus

--- app/src/main/java/open/sqxx/vostok27/model/repository/BluetoothFront.kt
+++ app/src/main/java/open/sqxx/vostok27/model/repository/BluetoothFront.kt
@@ -1,11 +1,10 @@
 package open.sqxx.vostok27.model.repository
 
-import app.akexorcist.bluetotohspp.library.BluetoothState
 import open.sqxx.vostok27.extension.rx.Variable
 
 class BluetoothFront {
 
 	val receiver = Variable(byteArrayOf())
 	val sender = Variable(byteArrayOf())
-	val status = Variable(BluetoothState.STATE_NONE)
+	val status = Variable(BluetoothStatus.NONE)
 }
---
+++ app/src/main/java/open/sqxx/vostok27/model/repository/BluetoothStatus.kt
@@ -0,0 +1,8 @@
+package open.sqxx.vostok27.model.repository
+
+enum class BluetoothStatus {
+	NONE,
+	CONNECTED,
+	WAIT_FOR_READY,
+	READY
+}
