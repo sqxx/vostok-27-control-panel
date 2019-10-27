8d1c2fbe885602d35a6d30ee168202155512d5b5
Valery Klachkov <sqxx.kv@gmail.com>
2019-10-27 19:13:57 +1000

    BluetoothModel improvements

--- app/src/main/java/open/sqxx/vostok27/model/repository/BluetoothModel.kt
+++ app/src/main/java/open/sqxx/vostok27/model/repository/BluetoothModel.kt
@@ -4,14 +4,15 @@ class BluetoothModel {
 	@ExperimentalUnsignedTypes
 	companion object {
 
-		const val PACKAGE_SIZE = 8
+		const val PACKAGE_SIZE = 10 - 2
 
-		const val MAGIC_BYTE = 0xF4.toByte()
+		const val START_MAGIC = 0xF4.toByte()
+		const val END_CR = 0x0D.toByte()
+		const val END_LF = 0x0A.toByte()
 
 		const val _P_STARTUP = 0x01.toByte()
 		const val _P_INIT_COMPLETE = 0x02.toByte()
 		const val _P_NOT_READY = 0x03.toByte()
-		const val _P_UNKNOWN_CMD = 0x0F.toByte()
 
 		const val _P_REQ_CO2 = 0xA1.toByte()
 		const val _P_REQ_HUM = 0xA2.toByte()
@@ -19,12 +20,40 @@ class BluetoothModel {
 		const val _P_REQ_PRES = 0xA4.toByte()
 		const val _P_REQ_SOLAR_PANELS_EF = 0xA5.toByte()
 
+		const val _P_CODE_SUCCESS = 0x00.toByte()
+		const val _P_CODE_FAILURE = 0xFF.toByte()
+
+		const val _P_SYSTEM_ENABLED = 0x00.toByte()
+		const val _P_SYSTEM_DISABLED = 0xFF.toByte()
+
+		const val _P_SWITCH_PRES_RELIEF_VALVE = 0xB1.toByte()
+		const val _P_STATUS_PRES_RELIEF_VALVE = 0xB2.toByte()
+		const val _P_SWITCH_PUMP_VALVE = 0xB3.toByte()
+		const val _P_STATUS_PUMP_VALVE = 0xB4.toByte()
+		const val _P_SWITCH_PROD_CO2 = 0xB5.toByte()
+		const val _P_STATUS_PROD_CO2 = 0xB6.toByte()
+
+		const val _P_SET_TIME = 0xD1.toByte()
+		const val _P_SET_DAY_TIME = 0xD2.toByte()
+		const val _P_SET_NIGHT_TIME = 0xD3.toByte()
+		const val _P_GET_TIME = 0xD4.toByte()
+		const val _P_GET_DAY_TIME = 0xD5.toByte()
+		const val _P_GET_NIGHT_TIME = 0xD6.toByte()
+		const val _P_SERIAL_RESET = 0xEA.toByte()
+
+		const val _PE_GAS_LEAK = 0xE1.toByte()
+		const val _PE_PACKAGE_ERR = 0xE3.toByte()
+		const val _PE_PACKAGE_CRC = 0xE4.toByte()
+		const val _PE_UNKNOWN_CMD = 0xE5.toByte()
+		const val _PE_PACKAGE_ERR_CRLF = 0xDA.toByte()
+		const val _PE_PACKAGE_ERR_MAGIC = 0xF4.toByte()
+
 		val VALUES_COMMANDS = byteArrayOf(
 			_P_REQ_CO2,
 			_P_REQ_HUM,
 			_P_REQ_TEMP,
-			_P_REQ_PRES
-			//_P_REQ_SOLAR_PANELS_EF
+			_P_REQ_PRES,
+			_P_REQ_SOLAR_PANELS_EF
 		)
 
 		fun calculateCrc(data: ByteArray): Int {
@@ -39,10 +68,10 @@ class BluetoothModel {
 
 		fun requestData(btFront: BluetoothFront, cmd: Byte) {
 			btFront.sender.value = byteArrayOf(
-				MAGIC_BYTE,
+				START_MAGIC,
 				cmd,
 				0, 0, 0, 0,
-				cmd, 0
+				0, cmd
 			)
 		}
 
@@ -54,8 +83,8 @@ class BluetoothModel {
 			 * что ведёт к потере оригинальных данных
 			 */
 
-			val lowByte = data[PACKAGE_SIZE - 2].toUByte().toInt()
-			val highByte = data[PACKAGE_SIZE - 1].toUByte().toInt()
+			val highByte = data[PACKAGE_SIZE - 2].toUByte().toInt()
+			val lowByte = data[PACKAGE_SIZE - 1].toUByte().toInt()
 
 			return highByte.shl(8) or lowByte
 		}
@@ -66,10 +95,10 @@ class BluetoothModel {
 			val b3 = data[4].toUByte().toInt()
 			val b4 = data[5].toUByte().toInt()
 
-			return b1 or
-				(b2.shl(8)) or
-				(b3.shl(16)) or
-				(b4.shl(24))
+			return b4 or
+				(b3.shl(8)) or
+				(b2.shl(16)) or
+				(b1.shl(24))
 		}
 	}
 }
