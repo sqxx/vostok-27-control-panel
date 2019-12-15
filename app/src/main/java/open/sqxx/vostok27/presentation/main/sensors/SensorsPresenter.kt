a5083f8b6ab6123adc62a8533edc3bddb34c9c95
sqxx <sqxx.kv@gmail.com>
2019-12-15 12:28:21 +1000

    [presentation/main] Added other commands to handle data in SensorsPresenter

--- app/src/main/java/open/sqxx/vostok27/presentation/main/sensors/SensorsPresenter.kt
+++ app/src/main/java/open/sqxx/vostok27/presentation/main/sensors/SensorsPresenter.kt
@@ -4,59 +4,42 @@ import com.arellomobile.mvp.InjectViewState
 import open.sqxx.vostok27.model.repository.BluetoothFront
 import open.sqxx.vostok27.model.repository.BluetoothModel
 import open.sqxx.vostok27.presentation.main.BluetoothFragmentPresenter
-import timber.log.Timber
 
 @ExperimentalUnsignedTypes
 @InjectViewState
 class SensorsPresenter(btFront: BluetoothFront) :
 	BluetoothFragmentPresenter<SensorsView>(btFront) {
 
-	override fun handleData(data: UByteArray) {
+	override fun handleData(data: UByteArray): Boolean {
+		if (!super.handleData(data)) return false
+
 		BluetoothModel.let {
 
 			val command = data[1]
-
-			// Обработка команды сброса
-			// После отправки reset будут ещё некоторое время приходить битые пакеты
-			it.handleReset(btFront, data)
-			if (it.isResetRequested())
-				return
-
-			val status = it.checkPackage(data)
-
-			// Обработка несоответствия магического числа
-			if (it.handleMagicByteError(btFront, status)) {
-				Timber.e("Несовпадение магического числа")
-				return
-			}
-
-			// Обработка несоответствия контрольной суммы
-			if (it.handleCRCError(btFront, status)) {
-				Timber.e("Несовпадение контрольной суммы")
-				return
-			}
-
-			// Обработка исключений по протоколу
-			if (it.handleProtocolExceptions(btFront, data)) {
-				Timber.e("Ошибки на стороне slave")
-				return
-			}
-
-			// Обработка ответов
 			val value = it.extractValue(data)
+
 			when (command) {
-				it._P_REQ_CO2 -> {
+				it._P_REQ_CO2         -> {
 					viewState.showCO2(value.toInt())
 				}
-				it._P_REQ_HUM -> {
+				it._P_REQ_HUM         -> {
 					viewState.showHumidity(value.toInt())
 				}
-				it._P_REQ_TEMP -> {
+				it._P_REQ_TEMP        -> {
 					viewState.showTemp(value.toInt())
 				}
-				it._P_REQ_PRES -> {
+				it._P_REQ_PRES        -> {
 					// Преобразуем мБары в Бары
 					viewState.showPressure(value.toFloat() / 1000f)
+				}
+				it._P_BATTERY_VOLTAGE -> {
+					viewState.showVoltage(value.toFloat() / 100f)
+				}
+				it._P_ENERGY_USAGE    -> {
+					viewState.showEnergyUsage(value.toFloat() / 100f)
+				}
+				it._P_ENERGY_GEN      -> {
+					viewState.showEnergyGen(value.toInt())
 				}
 			}
 
@@ -64,5 +47,7 @@ class SensorsPresenter(btFront: BluetoothFront) :
 			if (command == it.VALUES_COMMANDS.last())
 				it.requestAllSensorsData(btFront)
 		}
+
+		return true
 	}
 }
